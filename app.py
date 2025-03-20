aab520cfa385cf59c3ebba51ef127e638ae80a3c
DonatasNoreika <donoras@gmail.com>
2025-03-20 18:40:45 +0200

    Padariau, kad spausdintų žvaigždutes vietoje skaičių

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ size = int(input("Įveskite trikampio dydį: "))
 
 for i in range(1, size + 1):
     for j in range(1, i + 1):
-        print(j, end=" ")
+        print("*", end=" ")
     print()  # pereiname į naują eilutę
